442b3ed119663917f245760354bc0b037932f890
Maksymilian Chrapkowicz <maksymilian.chrapkowicz@gmail.com>
2023-03-14 18:21:20 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 {
     const tasks = [
         {
-            content: "aaa"
+            content: ""
         },
         {
-            content: "aaa"
+            content: ""
         },
 
     ];
@@ -12,7 +12,7 @@
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
                 <li>
                 ${task.content}
@@ -22,10 +22,31 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
     };
 
     init();
